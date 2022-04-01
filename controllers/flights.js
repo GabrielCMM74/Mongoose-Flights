92f24416950dbe77944e4b373720141022a101fb
GabrielCMM74 <97140818+GabrielCMM74@users.noreply.github.com>
2022-03-31 22:48:50 -0400

    idk whats wrong

--- controllers/flights.js
+++ controllers/flights.js
@@ -9,22 +9,28 @@ module.exports = {
 
 function newFlight(req, res) {
     const newFlight = new Flight();
-    departDefault = newFlight.departs;
+    let departDefault = newFlight.departs;
     const departsDate = departDefault.toISOString().slice(0, 16);
     res.render('flights/new', { departsDate });
 }
 
 
-function create(req, res) {
-    const flight = new Flight(req.body);
-    flight.save(function (err) {
-        if (err) return res.render('flights/new');
-        res.redirect('/flights/new');
-    });
-}
+// function create(req, res) {
+//     const flight = new Flight(req.body);
+//     flight.save(function (err) {
+//         if (err) return res.redirect('/flights/new');
+//     });
+// }
 
+function create(req, res){
+    Flight.create(req.body, function(err, flightDoc){
+        res.redirect('/flights')
+    })
+
+}
 function index(req, res) {
-    Flight.find({}, function (err, flights) {
-        res.render('flights/index', { flights });
+    Flight.find({}, function (err, flightDoc) {
+        
+        res.render('flights/index', { flights:flightDoc });
     });
 }
--- routes/flights.js
+++ routes/flights.js
@@ -3,10 +3,10 @@ var router = express.Router();
 const flightsCtrl = require('../controllers/flights')
 
 // GET /flights/new
-router.get('/new', flightsCtrl.new);
-	
 router.get('/', flightsCtrl.index)
 
+router.get('/new', flightsCtrl.new);
+	
 router.post('/', flightsCtrl.create)
 
 
